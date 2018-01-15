5c40b498a37e3fcbeb9e60e25d184582e6ccad3c
Vitaliy Romaniuk <komatozische@gmail.com>
2018-01-15 16:20:09 +0200

    Create new class Film, Main, FilmLibrary

---
+++ .idea/src/FilmLibrary.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * Created by Komatoz on 15.01.2018.
+ */
+public class FilmLibrary {
+    public LinkedList<Film> films;
+
+
+
+
+
+}
---
+++ .idea/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Komatoz on 13.01.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
